a8776a330e5618f0b7a6a61d9376a9f4a44aa340
Erick Pérez <erick.leao.perez@gmail.com>
2022-08-23 14:37:34 -0600

    add sections for the project

---
+++ src/components/filters.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const FiltersStyled = styled.div``;
+
+function Filters() {
+  return <FiltersStyled>Filters</FiltersStyled>;
+}
+
+export default Filters;
---
+++ src/components/repo-list.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const RepoListStyled = styled.div``;
+
+function RepoList() {
+  return <RepoListStyled>RepoList</RepoListStyled>;
+}
+
+export default RepoList;
